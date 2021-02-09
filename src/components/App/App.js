a388ea9349e97f8f7ad068df7533a15158c4427e
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-09 03:47:53 +0200

    Modified src/components/App/App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,44 @@
 import './App.css';
 import Header from '../Header/Header'
+import InfoBox from '../InfoBoxes/InfoBox'
+import Map from '../Map/Map'
+
+import { Card, CardContent } from '@material-ui/core'
 
 function App() {
   return (
     <div className="app">
 
-      {/* Header */}
-      <Header/>
 
-      {/* Title + Select input dropdown field */}
+      <div className='app__left'>
+        {/* Header */}
+        <Header/>
 
-      {/* Info Boxes */}
-      {/* Info Boxes */}
-      {/* Info Boxes */}
+        <div className='app__InfoBox__stats'>
+          {/* Info Boxes Corona Virus Cases*/}
+          <InfoBox title='Coronavirus cases' cases={123} total={2345}/>
+          {/* Info Boxes Recovered*/}
+          <InfoBox title='Recovered' cases={1234} total={24345}/>
+          {/* Info Boxes Deaths*/}
+          <InfoBox title='Deaths' cases={1235} total={23450}/>
+        </div>
 
-      {/* Table */}
-      {/* Graph */}
+        {/* Map */}
+        <Map />
+        
+      </div>
+        
 
-      {/* Map */}
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live cases by country</h3>
+          {/* Table */}
+          <h3>Worldwide new cases</h3>
+          {/* Graph */}
+        </CardContent>
+      </Card>
 
+      
     </div>
   );
 }
