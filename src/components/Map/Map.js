53d949287b02975a7bc69d2fb82b0b6a1585039c
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-09 03:49:43 +0200

    Added src/components/Map/

---
+++ src/components/Map/Map.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Map.css'
+
+function Map() {
+    return (
+        <div className='app__map'>
+            <h2>I am a map</h2>
+        </div>
+    )
+}
+
+export default Map
